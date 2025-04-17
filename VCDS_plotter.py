bbb675c5ffb25c0203c10613dcc0923e38c41aba
Charlie Nourse <cwnourse3@gmail.com>
2025-04-17 16:13:41 -0400

    Initial commit

---
+++ VCDS_plotter.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 17 16:02:18 2025
+
+Begin working on a VCDS log viewer with interactive plotting.
+ - import data series from a log
+ - plot results simply by selecting X-axis variable, Y-axis variable(s)
+ - interactive plot tool GUI for no-code analysis
+
+@author: noursec
+"""
+
